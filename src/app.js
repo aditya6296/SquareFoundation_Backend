6e75d6cfaebed0351a019ba3f7712c389a2c2d8c
Aditya kumar <adittyakumar721@gmail.com>
2025-02-25 14:42:58 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -13,16 +13,19 @@ const cookieParser = require("cookie-parser");
 const { verifyToken } = require("../middleware/jwtVerification.js");
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
+    methods: "GET, POST, PUT, PATCH, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
   })
 );
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/auth/otp", otpRouter);
+app.use("/api/v1/submit-form", formDB);
 app.use(verifyToken);
 // For checking authorization
 app.get("/api/v1/isAuthenticated", (req, res) => {
@@ -31,8 +34,6 @@ app.get("/api/v1/isAuthenticated", (req, res) => {
     isAuthenticated: true,
   });
 });
-
-app.use("/api/v1/submit-form", formDB);
 
 // app.use("/api/v1/auth/verify", authVerify);
 
