bc50f5ba024d358990c557ab485512b4fde3061d
Aditya kumar <adittyakumar721@gmail.com>
2025-02-25 15:07:51 +0530

    temperary allow cors

--- src/app.js
+++ src/app.js
@@ -13,14 +13,15 @@ const cookieParser = require("cookie-parser");
 const { verifyToken } = require("../middleware/jwtVerification.js");
 const app = express();
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-    credentials: true,
-    methods: "GET, POST, PUT, PATCH, DELETE",
-    allowedHeaders: "Content-Type, Authorization",
-  })
-);
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+//     credentials: true,
+//     methods: "GET, POST, PUT, PATCH, DELETE",
+//     allowedHeaders: "Content-Type, Authorization",
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
