e281ca87db97c18517dc3a342165f7a97c0a1625
Aditya kumar <adittyakumar721@gmail.com>
2025-02-25 15:22:32 +0530

    Allowed specific cors

--- src/app.js
+++ src/app.js
@@ -13,22 +13,22 @@ const cookieParser = require("cookie-parser");
 const { verifyToken } = require("../middleware/jwtVerification.js");
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-//     credentials: true,
-//     methods: "GET, POST, PUT, PATCH, DELETE",
-//     allowedHeaders: "Content-Type, Authorization",
-//   })
-// );
 app.use(
   cors({
-    origin: "*",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     methods: "GET, POST, PUT, PATCH, DELETE",
     allowedHeaders: "Content-Type, Authorization",
   })
 );
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//     methods: "GET, POST, PUT, PATCH, DELETE",
+//     allowedHeaders: "Content-Type, Authorization",
+//   })
+// );
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
