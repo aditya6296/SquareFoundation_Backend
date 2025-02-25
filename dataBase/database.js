be7574ca8d610acd88a813bb99454c032099c7d7
Aditya kumar <adittyakumar721@gmail.com>
2025-02-25 12:10:19 +0530

    changes env

--- dataBase/database.js
+++ dataBase/database.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
-const DBUrl = process.env.MOGO_DB_URL;
+const DBUrl = process.env.MONGO_DB_URL;
+const dbPassword = process.env.dbPass;
 
-let pass = DBUrl.replace("password", process.env.dbPass);
+const pass = DBUrl.replace("<db_password>", dbPassword);
 console.log(pass);
 mongoose
   .connect(pass, {
-    name: process.env.MONGO_DB_DATABASE_NAME,
+    dbName: process.env.MONGO_DB_DATABASE_NAME,
   })
 
   .then(() => {
